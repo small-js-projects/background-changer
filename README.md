02d1456fe9a29e8b2c2b2627aca33a490566a8c7
Viraj Patel <virajpatel@Virajs-MacBook-Pro.local>
2021-04-16 20:27:53 -0700

    feat: adds readme file

---
+++ README.md
@@ -0,0 +1,3 @@
+## This is a simple background color change app made using JavaScript, JQuery, HTML, and CSS
+---
+### Click the "Click Me!" button on the center of the page to change the background color!
--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// constant variable that never change and are accessible anywhere in our code
 const red = '#80ced6'
 const blue = '#d5f4e6'
 const green = '#f18973'
@@ -5,6 +6,7 @@ const pink = '#ff57b9'
 const purple = '#a0a2ff'
 const violet = '#6dfffa'
 
+// Object populated with color values defined above
 const rgbValues = [
     red,
     blue,
@@ -14,8 +16,7 @@ const rgbValues = [
     violet,
 ]
 
-
-
+// caches element references - parts of the dom we need to touch
 const $background = $('#body');
 const $click = $('#click');
 const $container = $('#container');
@@ -23,11 +24,10 @@ const $container = $('#container');
 const clickMe = document.getElementById("body");
 
 // Event listeners
-
 $click.on('click', changeColor);
 
-
-
+// This function generates a random integer in range 0 to rgbValues.length and 
+// sets the HTML body's background color to a random index value in the rgbValues object
 function changeColor(){
     const randomColor = Math.floor(Math.random() * rgbValues.length)
     clickMe.style.background = rgbValues[randomColor];
